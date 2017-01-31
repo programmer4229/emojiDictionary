b606819251d795f641969fd45b8cee261cd7635c
Gaby G <GabyG@Gabriellas-Air.home>
2017-01-30 21:41:40 -0500

    Initial commit

---
+++ emojiDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojiDictionary
+//
+//  Created by Gabriella Gonzalez on 1/30/17.
+//  Copyright © 2017 Gaby G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
