97cc3b2b7b4fabb09e38de062c0166a90f831bb9
Gaby G <GabyG@Gabriellas-Air.home>
2017-01-31 16:58:23 -0500

    each row in TableView now shows a different emoji based on the array

--- emojiDictionary/ViewController.swift
+++ emojiDictionary/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var emojiTable: UITableView!
     
+    var emojis  = ["💩","😎","😈","😂","😘"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,12 +23,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+       
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "😎"
+        cell.textLabel?.text = emojis[indexPath.row]
             
         return cell
     }
