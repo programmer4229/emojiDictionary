8602d973f2975e32bb1ebb454ee4366db1bb5c56
Gaby G <GabyG@Gabriellas-Air.home>
2017-01-31 21:03:36 -0500

    definitions correspond to emojis, app is DONE!!!!

--- emojiDictionary/DefinitionViewController.swift
+++ emojiDictionary/DefinitionViewController.swift
@@ -1,32 +1,45 @@
  //
-//  DefinitionViewController.swift
-//  emojiDictionary
-//
-//  Created by Gabriella Gonzalez on 1/31/17.
-//  Copyright © 2017 Gaby G. All rights reserved.
-//
-
-import UIKit
-
-class DefinitionViewController: UIViewController {
-
+ //  DefinitionViewController.swift
+ //  emojiDictionary
+ //
+ //  Created by Gabriella Gonzalez on 1/31/17.
+ //  Copyright © 2017 Gaby G. All rights reserved.
+ //
+ 
+ import UIKit
+ 
+ class DefinitionViewController: UIViewController {
+    
     @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var emojiDefinition: UILabel!
     
     var emoji = "NO EMOJI"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         emojiLabel.text = emoji
+        
+        if emoji == "💩" {
+            emojiDefinition.text = "something gross"
+        }else if emoji == "😎"{
+            emojiDefinition.text = "a cool person"
+        }else if emoji == "😈" {
+            emojiDefinition.text = "a happy evil person"
+        }else if emoji == "😂" {
+            emojiDefinition.text = "a person in tears from laughing too hard"
+        }else if emoji == "😘" {
+            emojiDefinition.text = "a person blowing a kiss"
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-
-}
+    
+    
+ }
